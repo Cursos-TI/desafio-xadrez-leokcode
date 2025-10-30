0ab2e0f2ed294f750cfa0a7bb185d9f7ca2a289e
leokcode <leok.code@gmail.com>
2025-10-30 17:02:19 +0000

    Fiz o Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,7 @@ int main(){
     int movimentosTorre = 5;            // Variável com o número de movimentos que a Torre irá fazer.
     int movimentosBispo = 5;            // Variável com o número de movimentos que o Bispo irá fazer.
     int movimentosRainha = 8;           // Variável com o número de movimentos que a Rainha irá fazer.
+    int movimentosCavalo = 1;           // Variável com o número de movimentos que o Cavalo irá fazer.
     char cima[20] = "↑Cima↑";           // Variável que exibe o movimento para cima.
     char baixo[20] = "↓Baixo↓";         // Variável que exibe o movimento para baixo.
     char esquerda[20] = "←Esquerda←";   // Variável que exibe o movimento para esquerda.
@@ -53,9 +54,26 @@ int main(){
         contador++;
     } while (contador < movimentosRainha);
 
+    contador = 0; // Zera o contador após a execução do do-while.
+    printf("\n"); // Cria uma linha vazia para separar a exibição dos movimentos de cada peça.
 
+
+    // Área da lógica para simular o movimento do Cavalo.
+    // O Cavalo se move duas casas em uma direção (horizontal ou vertical) e depois uma casa perpendicularmente, formando um "L".
+    // Moveremos duas casas para  baixo e uma casa a esquerda usando o while e o for aninhados.
+    printf("=-=-= CAVALO =-=-=\n");
+    printf("Movendo em L (%d casas para baixo e %d casa a esquerda):\n", (movimentosCavalo + 1), movimentosCavalo);
+        while (movimentosCavalo--){ // Faz o loop e decrementa até a variável movimentoCavalo chegar a 0 e para.
+            for (contador; contador < 2; contador++) // Faz o loop com o contador zerado e incrementa até 
+                                                     // o contador chegar a 2 e para.
+            {
+                printf("%s ,", baixo); // Imprime duas vezes "↓Baixo↓"
+            }
+            printf("%s\n", esquerda); // Imprime uma vez "←Esquerda←"
+        }
     printf("\n");
-    printf("=-=-= FIM DA SIMULAÇÂO =-=-=");
+
+    printf(" =-=-= FIM DA SIMULAÇÃO =-=-=");
 
     return 0;
 
